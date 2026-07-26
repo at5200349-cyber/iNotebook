df3cd568142e48456bf8b3580c33bc95e2427841
at5200349-cyber <at5200349@gmail.com>
2026-07-26 17:00:32 +0530

    create user with token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,56 @@
 const express=require('express');
 const router=express.Router();
 const User =require('../models/User');
-//Create a User :Post "/api/auth". Doesn't reqire auth;
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=new User(req.body);
-    user.save();
+const { body, validationResult } = require("express-validator");
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET="amanthakur"
+
+//Create a User : post api . No login requuired
+router.post('/Createuser',[
+     body ('name').isLength({min:3}),
+    body ('email').isEmail(),
+    body('password').isLength({min:8})
+
+
+],async(req,res)=>{
+  // if there are error return badrequset with error
+    const result = validationResult(req);
+
+    if (!result.isEmpty()) {
+      return res.status(400).json({
+        errors: result.array(),
+      });
+    }
+    try{
+    let user= await User.findOne({email:req.body.email});
+    if(user){
+
+      return res.status(400).json({error:"Sorry a user with this email already exiss"})
+    }
+    const salt = await bcrypt.genSalt(10);
+
+
+     const secpass=await bcrypt.hash(req.body.password,salt);
+   user=await User.create({
+       name:req.body.name,
+        email:req.body.email,
+        password:secpass,
+    });
+    const data={
+      user:{
+        id:user.id
+      }
+    }
+    const token=jwt.sign(data ,JWT_SECRET,{expiresIn:"1d"})
+    // console.log(token)
+    res.json(token);}
+    catch(error){
+     console.log(error.message);
+     res.status(500).send("Some Error occured")
+    }
     
-    res.send(req.body);
-    
+  
+
 })
 module.exports=router
